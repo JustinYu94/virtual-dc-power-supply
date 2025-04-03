7703d30b4237fbff6c2ed2b781abcb34a89ec29c
justin.yu <zxcandy1234@gmail.com>
2025-04-03 22:28:57 +0800

    Add implementation and improve naming.

--- src/vdc_core/vdc_core.c
+++ src/vdc_core/vdc_core.c
@@ -1,8 +1,14 @@
+#include <assert.h>
+#include <string.h>
 #include "vdc_core.h"
 
-#define VDC_MODEL_SIZE         32
+#define VDC_ASSERT(x)       assert(x)
 
-typedef struct virtual_dc_power_supply {
+#define VDC_MODEL_SIZE          32
+#define VDC_INVALID_SLOT        (-1)
+#define VDC_INVALID_HANDLE      0
+
+typedef struct _vdc_t {
     float voltage;
     float current;
     float min_voltage;
@@ -10,98 +16,325 @@ typedef struct virtual_dc_power_supply {
     float min_current;
     float max_current;
     bool output_state;
-    char model[VDC_MODEL_SIZE];
-} virtual_dc_power_supply;
+    char* model;
+    const vdc_uut_t* uut;
+} _vdc_t;
 
-typedef enum vdc_slot_state {
+typedef enum _vdc_slot_state {
     VDC_SLOT_STATE_FREE,
     VDC_SLOT_STATE_CREATED,
-} vdc_slot_state;
+} _vdc_slot_state;
 
-typedef struct vdc_slot {
-    virtual_dc_power_supply vdc;
-    vdc_slot_state state;
-} vdc_slot;
+typedef struct _vdc_slot_t {
+    _vdc_t instance;
+    _vdc_slot_state state;
+} _vdc_slot_t;
 
-static vdc_slot _vdc_pool[VDC_MAX_HANDLE];
+static _vdc_slot_t _vdc_pool[VDC_MAX_HANDLE];
 
-static int _vdc_search_free_slot()
+static _vdc_slot_t* _vdc_slot_at(vdc_handle_t handle)
 {
-    return -1;
+    if (handle == VDC_INVALID_HANDLE || handle > VDC_MAX_HANDLE)
+    {
+        return NULL;
+    }
+    return &_vdc_pool[handle - 1];
 }
 
-vdc_result vdc_create(vdc_handle *handle)
+static _vdc_t* _vdc_lookup_instance(vdc_handle_t handle)
+{
+    if (handle < 1 || handle > VDC_MAX_HANDLE)
+    {
+        return NULL;
+    }
+
+    _vdc_slot_t* slot = &_vdc_pool[handle - 1];
+    if (slot->state == VDC_SLOT_STATE_CREATED)
+    {
+        return &slot->instance;
+    }
+    return NULL;
+}
+
+static void _vdc_init_instance_model_a(_vdc_t* vdc)
+{
+    VDC_ASSERT(vdc);
+
+    vdc->min_voltage = 0.0;
+    vdc->max_voltage = 15.0;
+
+    vdc->min_current = 0.0;
+    vdc->max_current = 4.0;
+
+    vdc->output_state = false;
+    vdc->model = "MODE-A";
+    vdc->uut = NULL;
+}
+
+vdc_result_t vdc_create(vdc_handle_t *handle)
 {
     if (!handle)
     {
         return VDC_ERROR_PARAM_NULL;
     }
 
-    int free_slot_index = _vdc_search_free_slot();
-    if (free_slot_index == -1)
+    int slot_index = -1;
+    for (int i = 0; i < VDC_MAX_HANDLE; i++)
+    {
+        if (_vdc_pool[i].state == VDC_SLOT_STATE_FREE)
+        {
+            slot_index = i;
+            break;;
+        }
+    }
+    if (slot_index == -1)
     {
         return VDC_ERROR_MAX_HANDLE;
     }
 
+    _vdc_slot_t* slot = &_vdc_pool[slot_index];
+    slot->state = VDC_SLOT_STATE_CREATED;
+    _vdc_t* vdc = &slot->instance;
+    _vdc_init_instance_model_a(vdc);
+    *handle = slot_index + 1;
+
+    return VDC_SUCCESS;
+}
+
+vdc_result_t vdc_destroy(vdc_handle_t handle)
+{
+    _vdc_slot_t* slot = _vdc_slot_at(handle);
+    if (!slot)
+    {
+        return VDC_ERROR_INVALID_HANDLE;
+    }
+    slot->state = VDC_SLOT_STATE_FREE;
+    return VDC_SUCCESS;
+}
+
+vdc_result_t vdc_get_model(vdc_handle_t handle, char* model, size_t* model_size)
+{
+    _vdc_t* vdc = _vdc_lookup_instance(handle);
+    if (!vdc)
+    {
+        return VDC_ERROR_INVALID_HANDLE;
+    }
+    if (!model_size)
+    {
+        return VDC_ERROR_PARAM_NULL;
+    }
+
+    size_t required_size = strlen(vdc->model) + 1;
+    if (model == NULL)
+    {
+        *model_size = required_size;
+        return VDC_SUCCESS;
+    }
+    if (*model_size < required_size)
+    {
+        return VDC_ERROR_BUFFER_TOO_SMALL;
+    }
+
+    strncpy_s(model, *model_size, vdc->model, required_size);
+    return VDC_SUCCESS;
+}
+
+vdc_result_t vdc_get_voltage_spec(vdc_handle_t handle, vdc_param_spec_t *spec)
+{
+    _vdc_t* vdc = _vdc_lookup_instance(handle);
+    if (!vdc)
+    {
+        return VDC_ERROR_INVALID_HANDLE;
+    }
+    if (!spec)
+    {
+        return VDC_ERROR_PARAM_NULL;
+    }
+
+    spec->min = vdc->min_voltage;
+    spec->max = vdc->max_voltage;
+    return VDC_SUCCESS;
+}
+
+vdc_result_t vdc_get_current_spec(vdc_handle_t handle, vdc_param_spec_t *spec)
+{
+    _vdc_t* vdc = _vdc_lookup_instance(handle);
+    if (!vdc)
+    {
+        return VDC_ERROR_INVALID_HANDLE;
+    }
+    if (!spec)
+    {
+        return VDC_ERROR_PARAM_NULL;
+    }
+
+    spec->min = vdc->min_current;
+    spec->max = vdc->max_current;
     return VDC_ERROR_INTERNAL;
 }
 
-vdc_result vdc_destroy(vdc_handle handle)
+vdc_result_t vdc_set_voltage(vdc_handle_t handle, float value)
 {
-    return VDC_ERROR_INTERNAL;
+    _vdc_t* vdc = _vdc_lookup_instance(handle);
+    if (!vdc)
+    {
+        return VDC_ERROR_INVALID_HANDLE;
+    }
+    if (value < vdc->min_voltage || value > vdc->max_voltage)
+    {
+        return VDC_ERROR_PARAM_INVALID;
+    }
+
+    vdc->voltage = value;
+    return VDC_SUCCESS;
 }
 
-vdc_result vdc_get_model(vdc_handle handle, char* model, size_t model_size)
+vdc_result_t vdc_get_voltage(vdc_handle_t handle, float *value)
 {
-    return VDC_ERROR_INTERNAL;
+    _vdc_t* vdc = _vdc_lookup_instance(handle);
+    if (!vdc)
+    {
+        return VDC_ERROR_INVALID_HANDLE;
+    }
+    if (!value)
+    {
+        return VDC_ERROR_PARAM_NULL;
+    }
+
+    *value = vdc->voltage;
+    return VDC_SUCCESS;
 }
 
-vdc_result vdc_get_voltage_spec(vdc_handle handle, vdc_param_spec *spec)
+vdc_result_t vdc_set_current(vdc_handle_t handle, float value)
 {
-    return VDC_ERROR_INTERNAL;
+    _vdc_t* vdc = _vdc_lookup_instance(handle);
+    if (!vdc)
+    {
+        return VDC_ERROR_INVALID_HANDLE;
+    }
+    if (value < vdc->min_current || value > vdc->max_current)
+    {
+        return VDC_ERROR_PARAM_INVALID;
+    }
+
+    vdc->current = value;
+    return VDC_SUCCESS;
 }
 
-vdc_result vdc_get_current_spec(vdc_handle handle, vdc_param_spec *spec)
+vdc_result_t vdc_get_current(vdc_handle_t handle, float *value)
 {
-    return VDC_ERROR_INTERNAL;
+    _vdc_t* vdc = _vdc_lookup_instance(handle);
+    if (!vdc)
+    {
+        return VDC_ERROR_INVALID_HANDLE;
+    }
+    if (!value)
+    {
+        return VDC_ERROR_PARAM_NULL;
+    }
+
+    *value = vdc->current;
+    return VDC_SUCCESS;
 }
 
-vdc_result vdc_set_voltage(vdc_handle handle, float value)
+vdc_result_t vdc_set_output_state(vdc_handle_t handle, bool value)
 {
-    return VDC_ERROR_INTERNAL;
+    _vdc_t* vdc = _vdc_lookup_instance(handle);
+    if (!vdc)
+    {
+        return VDC_ERROR_INVALID_HANDLE;
+    }
+
+    vdc->output_state = value;
+    return VDC_SUCCESS;
 }
 
-vdc_result vdc_get_voltage(vdc_handle handle, float *value)
+vdc_result_t vdc_connect_uut(vdc_handle_t handle, const vdc_uut_t *uut)
 {
-    return VDC_ERROR_INTERNAL;
+    _vdc_t* vdc = _vdc_lookup_instance(handle);
+    if (!vdc)
+    {
+        return VDC_ERROR_INVALID_HANDLE;
+    }
+    if (!uut)
+    {
+        return VDC_ERROR_PARAM_NULL;
+    }
+    if (vdc->uut)
+    {
+        return VDC_ERROR_UUT_CONNECTED;
+    }
+
+    vdc->uut = uut;
+    return VDC_SUCCESS;
 }
 
-vdc_result vdc_set_current(vdc_handle handle, float value)
+vdc_result_t vdc_disconnect_uut(vdc_handle_t handle)
 {
-    return VDC_ERROR_INTERNAL;
+    _vdc_t* vdc = _vdc_lookup_instance(handle);
+    if (!vdc)
+    {
+        return VDC_ERROR_INVALID_HANDLE;
+    }
+
+    vdc->uut = NULL;
+    return VDC_SUCCESS;
 }
 
-vdc_result vdc_get_current(vdc_handle handle, float *value)
+vdc_result_t vdc_is_uut_connected(vdc_handle_t handle, bool* value)
 {
-    return VDC_ERROR_INTERNAL;
+    _vdc_t* vdc = _vdc_lookup_instance(handle);
+    if (!vdc)
+    {
+        return VDC_ERROR_INVALID_HANDLE;
+    }
+    if (!value)
+    {
+        return VDC_ERROR_PARAM_NULL;
+    }
+
+    *value = (vdc->uut != NULL);
+    return VDC_SUCCESS;
 }
 
-vdc_result vdc_set_output_state(vdc_handle handle, bool value)
+vdc_result_t vdc_read_status(vdc_handle_t handle, vdc_status_t *status)
 {
-    return VDC_ERROR_INTERNAL;
-}
+    _vdc_t* vdc = _vdc_lookup_instance(handle);
+    if (!vdc)
+    {
+        return VDC_ERROR_INVALID_HANDLE;
+    }
+    if (!status)
+    {
+        return VDC_ERROR_PARAM_NULL;
+    }
 
-vdc_result vdc_connect_uut(vdc_handle handle, const vdc_uut *uut)
-{
-    return VDC_ERROR_INTERNAL;
-}
+    status->output_state = vdc->output_state;
+    if (vdc->output_state == true)
+    {
+        if (vdc->uut)
+        {
+            vdc_output_power_t supply = { 0 };
+            vdc_output_power_t response = { 0 };
+            vdc->uut->supply_power(&supply, &response, vdc->uut->user_data);
 
-vdc_result vdc_disconnect_uut(vdc_handle handle, const vdc_uut *uut)
-{
-    return VDC_ERROR_INTERNAL;
-}
+            status->output_voltage = response.voltage;
+            status->output_current = response.current;
+            status->output_power = response.voltage * response.current;
+        }
+        else
+        {
+            status->output_voltage = vdc->voltage;
+            status->output_current = 0.0;
+            status->output_power = 0.0;
+        }
+    }
+    else
+    {
+        status->output_voltage = 0.0;
+        status->output_current = 0.0;
+        status->output_power = 0.0;
+    }
 
-vdc_result vdc_read_status(vdc_handle handle, vdc_status *status)
-{
-    return VDC_ERROR_INTERNAL;
+    return VDC_SUCCESS;
 }
--- src/vdc_core/vdc_core.h
+++ src/vdc_core/vdc_core.h
@@ -10,60 +10,62 @@ extern "C" {
 
 #define VDC_MAX_HANDLE          16
 
-typedef int32_t         vdc_result;
+typedef int32_t         vdc_result_t;
 
 #define VDC_SUCCESS                                 0
 #define VDC_ERROR_INTERNAL                          1L
 #define VDC_ERROR_MAX_HANDLE                        2L
 #define VDC_ERROR_INVALID_HANDLE                    3L
-#define VDC_ERROR_NOT_ENOUGH_BUFFER                 4L
+#define VDC_ERROR_BUFFER_TOO_SMALL                  4L
 #define VDC_ERROR_PARAM_INVALID                     5L
 #define VDC_ERROR_PARAM_NULL                        6L
+#define VDC_ERROR_UUT_CONNECTED                     7L
 
-typedef uint32_t         vdc_handle;
+typedef uint32_t         vdc_handle_t;
 
-typedef struct vdc_param_spec {
+typedef struct vdc_param_spec_t {
     float min;
     float max;
     float resolution;
-} vdc_param_spec;
+} vdc_param_spec_t;
 
-typedef struct vdc_status {
+typedef struct vdc_status_t {
     float output_voltage;
     float output_current;
     float output_power;
     bool output_state;
-} vdc_status;
+} vdc_status_t;
 
-typedef struct vdc_supplied {
+typedef struct vdc_output_power_t {
     float voltage;
     float current;
-} vdc_supplied;
+} vdc_output_power_t;
 
-typedef void (*vdc_supplied_callback)(const vdc_supplied *supply, vdc_supplied *response, void *user_data);
+typedef void (*vdc_supply_power_callback_t)(const vdc_output_power_t* supply, vdc_output_power_t* response, void* user_data);
 
-typedef struct vdc_uut {
-    vdc_supplied_callback uut_response;
+typedef struct vdc_uut_t {
+    vdc_supply_power_callback_t supply_power;
     void *user_data;
-} vdc_uut;
+} vdc_uut_t;
 
-vdc_result vdc_create(vdc_handle *handle);
-vdc_result vdc_destroy(vdc_handle handle);
+vdc_result_t vdc_create(vdc_handle_t *handle);
+vdc_result_t vdc_destroy(vdc_handle_t handle);
 
-vdc_result vdc_get_model(vdc_handle handle, char* model, size_t model_size);
-vdc_result vdc_get_voltage_spec(vdc_handle handle, vdc_param_spec *spec);
-vdc_result vdc_get_current_spec(vdc_handle handle, vdc_param_spec *spec);
+vdc_result_t vdc_get_model(vdc_handle_t handle, char* model, size_t* model_size);
+vdc_result_t vdc_get_voltage_spec(vdc_handle_t handle, vdc_param_spec_t* spec);
+vdc_result_t vdc_get_current_spec(vdc_handle_t handle, vdc_param_spec_t* spec);
 
-vdc_result vdc_set_voltage(vdc_handle handle, float value);
-vdc_result vdc_get_voltage(vdc_handle handle, float *value);
-vdc_result vdc_set_current(vdc_handle handle, float value);
-vdc_result vdc_get_current(vdc_handle handle, float *value);
-vdc_result vdc_set_output_state(vdc_handle handle, bool value);
+vdc_result_t vdc_set_voltage(vdc_handle_t handle, float value);
+vdc_result_t vdc_get_voltage(vdc_handle_t handle, float* value);
+vdc_result_t vdc_set_current(vdc_handle_t handle, float value);
+vdc_result_t vdc_get_current(vdc_handle_t handle, float* value);
+vdc_result_t vdc_set_output_state(vdc_handle_t handle, bool value);
 
-vdc_result vdc_connect_uut(vdc_handle handle, const vdc_uut *uut);
-vdc_result vdc_disconnect_uut(vdc_handle handle, const vdc_uut *uut);
+vdc_result_t vdc_connect_uut(vdc_handle_t handle, const vdc_uut_t* uut);
+vdc_result_t vdc_disconnect_uut(vdc_handle_t handle);
+vdc_result_t vdc_is_uut_connected(vdc_handle_t handle, bool* value);
 
-vdc_result vdc_read_status(vdc_handle handle, vdc_status *status);
+vdc_result_t vdc_read_status(vdc_handle_t handle, vdc_status_t* status);
 
 #ifdef __cplusplus
 }
